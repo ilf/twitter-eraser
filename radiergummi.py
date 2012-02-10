9c4fac3bab3024847175f3bce29f19dcdd8f3153
ilf <ilf@zeromail.org>
2012-02-10 18:15:31 +0100

    s/threshold/limit/, reduce output to stuff actually done

--- radiergummi.py
+++ radiergummi.py
@@ -14,45 +14,39 @@ consumer_secret = 'edit.this'
 key = 'edit.this'
 secret = 'edit.this'
 
-# thresholds to keep
-user_timeline_threshold = 15
-retweeted_by_me_threshold = 3
-favorites_threshold = 3
-direct_messages_threshold = 2
-sent_direct_messages_threshold = 2
+# limits to keep
+user_timeline_limit = 4
+retweeted_by_me_limit = 3
+favorites_limit = 3
+direct_messages_limit = 2
+sent_direct_messages_limit = 2
 
 # authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret, secure = True)
 auth.set_access_token(key, secret)
 api = tweepy.API(auth, secure = True)
-print "authenticated user:", api.me().screen_name
 
-print "deleting statuses, keeping", user_timeline_threshold
 user_timeline = tweepy.Cursor(api.user_timeline).items()
-for status in itertools.islice(user_timeline, user_timeline_threshold, None):
+for status in itertools.islice(user_timeline, user_timeline_limit, None):
 	api.destroy_status(status.id)
-	print "deleted:", status.id
+	print "deleted status:", status.id
 
-print "deleting retweets, keeping", retweeted_by_me_threshold
 retweeted_by_me = tweepy.Cursor(api.retweeted_by_me).items()
-for status in itertools.islice(retweeted_by_me, retweeted_by_me_threshold, None):
+for status in itertools.islice(retweeted_by_me, retweeted_by_me_limit, None):
 	api.destroy_status(status.id)
-	print "deleted:", status.id
+	print "deleted retweet:", status.id
 
-print "deleting favorites, keeping", favorites_threshold
 favorites = tweepy.Cursor(api.favorites).items()
-for status in itertools.islice(favorites, favorites_threshold, None):
+for status in itertools.islice(favorites, favorites_limit, None):
 	api.destroy_favorite(status.id)
-	print "deleted:", status.id
+	print "deleted favorite:", status.id
 
-print "deleting direct messages, keeping", direct_messages_threshold
 direct_messages = tweepy.Cursor(api.direct_messages).items()
-for direct_message in itertools.islice(direct_messages, direct_messages_threshold, None):
+for direct_message in itertools.islice(direct_messages, direct_messages_limit, None):
 	api.destroy_direct_message(direct_message.id)
-	print "deleted:", direct_message.id
+	print "deleted direct message:", direct_message.id
 
-print "deleting sent direct messages, keeping", sent_direct_messages_threshold
 sent_direct_messages = tweepy.Cursor(api.sent_direct_messages).items()
-for direct_message in itertools.islice(sent_direct_messages, sent_direct_messages_threshold, None):
+for direct_message in itertools.islice(sent_direct_messages, sent_direct_messages_limit, None):
 	api.destroy_direct_message(direct_message.id)
-	print "deleted:", direct_message.id
+	print "deleted sent direct message:", direct_message.id
