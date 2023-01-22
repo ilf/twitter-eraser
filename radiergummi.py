d5e7c708807a5cae96b76372b4d570c7cb0a8f9d
ilf <ilf@zeromail.org>
2023-01-22 13:15:29 +0100

    correct case

--- radiergummi.py
+++ radiergummi.py
@@ -21,29 +21,29 @@ access_token_secret = "get this from https://apps.twitter.com/"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+API = tweepy.API(auth)
 
-user_timeline = tweepy.Cursor(api.user_timeline).items()
+user_timeline = tweepy.Cursor(API.user_timeline).items()
 for status in itertools.islice(user_timeline, user_timeline_limit, None):
-	api.destroy_status(status.id)
+	API.destroy_status(status.id)
 	print("deleted status:", status.id)
 
-favorites = tweepy.Cursor(api.favorites).items()
+favorites = tweepy.Cursor(API.favorites).items()
 for status in itertools.islice(favorites, favorites_limit, None):
-	api.destroy_favorite(status.id)
+	API.destroy_favorite(status.id)
 	print("deleted favorite:", status.id)
 
-direct_messages = tweepy.Cursor(api.direct_messages).items()
+direct_messages = tweepy.Cursor(API.direct_messages).items()
 for direct_message in itertools.islice(direct_messages, direct_messages_limit, None):
-	api.destroy_direct_message(direct_message.id)
+	API.destroy_direct_message(direct_message.id)
 	print("deleted direct message:", direct_message.id)
 
-blocks = tweepy.Cursor(api.blocks).items()
+blocks = tweepy.Cursor(API.blocks).items()
 for block in itertools.islice(blocks, blocks_limit, None):
-	api.destroy_block(block.id)
+	API.destroy_block(block.id)
 	print("deleted block:", block.id)
 
-saved_searches = api.saved_searches()
+saved_searches = API.saved_searches()
 for saved_search in itertools.islice(saved_searches, saved_searches_limit, None):
-	api.destroy_saved_search(saved_search.id)
+	API.destroy_saved_search(saved_search.id)
 	print("deleted saved search:", saved_search.id)
